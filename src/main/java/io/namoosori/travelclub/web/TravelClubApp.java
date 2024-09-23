8a8eeb647d8dad7bb3e47b77e7c42228e0513ab1
tama <rufina130@naver.com>
2024-09-23 13:32:29 +0900

    travelclub-spring-web

--- src/main/java/io/namoosori/travelclub/web/TravelClubApp.java
+++ src/main/java/io/namoosori/travelclub/web/TravelClubApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelClubApp {
 
-    //commit 확인
     public static void main(String[] args) {
+
         SpringApplication.run(TravelClubApp.class);
     }
 }
