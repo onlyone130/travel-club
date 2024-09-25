4042c0f630ca41eb994165a697f6ae1cf244e587
tama <rufina130@naver.com>
2024-09-25 16:04:10 +0900

    MembershipController

--- src/main/java/io/namoosori/travelclub/web/controller/MembershipController.java
+++ src/main/java/io/namoosori/travelclub/web/controller/MembershipController.java
@@ -1,4 +1,25 @@
 package io.namoosori.travelclub.web.controller;
 
+import io.namoosori.travelclub.web.service.MembershipService;
+import io.namoosori.travelclub.web.service.sdo.MemberCdo;
+import io.namoosori.travelclub.web.service.sdo.MembershipCdo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MembershipController {
+
+    private MembershipService membershipService;
+
+    public MembershipController(MembershipService membershipService){
+        this.membershipService = membershipService;
+    }
+
+    @PostMapping //localhost:8090/member
+    public String register(@RequestBody MembershipCdo membershipCdo){
+        return membershipService.registerMembership(membershipCdo);
+    }
 }
