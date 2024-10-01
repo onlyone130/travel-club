a288bf9c473d55c2a3a03d7debb7b127e65e3221
tama <rufina130@naver.com>
2024-10-01 12:39:55 +0900

    @Repository 어노테이션 주석처리

--- src/main/java/io/namoosori/travelclub/web/store/mapstore/ClubMapStore.java
+++ src/main/java/io/namoosori/travelclub/web/store/mapstore/ClubMapStore.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Repository
+//@Repository
 public class ClubMapStore implements ClubStore {
 	//
 	private Map<String, TravelClub> clubMap;
