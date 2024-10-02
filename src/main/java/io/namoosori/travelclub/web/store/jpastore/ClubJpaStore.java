c73328682f740504692c772c4eed2fb085180561
tama <rufina130@naver.com>
2024-10-02 15:22:05 +0900

    update

--- src/main/java/io/namoosori/travelclub/web/store/jpastore/ClubJpaStore.java
+++ src/main/java/io/namoosori/travelclub/web/store/jpastore/ClubJpaStore.java
@@ -2,15 +2,26 @@ package io.namoosori.travelclub.web.store.jpastore;
 
 import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.store.ClubStore;
+import io.namoosori.travelclub.web.store.jpastore.jpo.TravelClubJpo;
+import io.namoosori.travelclub.web.store.jpastore.repository.ClubRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class ClubJpaStore implements ClubStore {
+
+    private ClubRepository clubRepository;
+
+    //생성자를 통한 주입을 받음. 그 인스턴스 객체는 SpringDataJPA가 만들어주는 것이다.
+    public ClubJpaStore(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
     @Override
     public String create(TravelClub club) {
-        return "";
+        clubRepository.save(new TravelClubJpo(club));
+        return club.getId();
     }
 
     @Override
--- src/main/java/io/namoosori/travelclub/web/store/jpastore/repository/ClubRepository.java
+++ src/main/java/io/namoosori/travelclub/web/store/jpastore/repository/ClubRepository.java
@@ -1,6 +1,7 @@
 package io.namoosori.travelclub.web.store.jpastore.repository;
 
+import io.namoosori.travelclub.web.store.jpastore.jpo.TravelClubJpo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClubRepository extends JpaRepository {
+public interface ClubRepository extends JpaRepository<TravelClubJpo, String> {
 }
