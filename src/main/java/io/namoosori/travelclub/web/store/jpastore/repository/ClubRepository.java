9d6d281cf29391916e514d582f43ace70d755937
tama <rufina130@naver.com>
2024-10-02 15:11:49 +0900

    create ClubRepository Interface

---
+++ src/main/java/io/namoosori/travelclub/web/store/jpastore/repository/ClubRepository.java
@@ -0,0 +1,6 @@
+package io.namoosori.travelclub.web.store.jpastore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository {
+}
